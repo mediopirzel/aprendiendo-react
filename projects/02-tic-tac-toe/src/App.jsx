1df4dca901af827bd8392a94dc5200853ae0f7a3
mediopirzel <xaviersoler@gmail.com>
2023-02-03 08:05:47 +0100

    tic tac toe, new project

---
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,21 @@
+const TURNS = {
+  X: 'x',
+  O: 'o',
+};
+
+const board = Array(9).fill(null);
+
+function App() {
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return <div key={index}>{index}</div>;
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default App;
